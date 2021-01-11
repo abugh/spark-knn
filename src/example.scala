ca4a4fbf708af240fcb99af301c8976dbdaa7617
abugh <527580050@qq.com>
2021-01-11 12:55:46 +0800

    png

--- src/example.scala
+++ src/example.scala
@@ -1,3 +1,16 @@
-object example {
+import scala.util.Random
 
+object example {
+  val embeddingSize = 300000
+  val vectorSize = 10
+  val topK = 20
+  val partitionNum = 200
+  // generate randomEmbedding, if you have your own embedding dataset, just use it instead.
+  val randomEmbedding = (0 to embeddingSize - 1).map{i => (i.toString, (0 to vectorSize - 1).map{j => Random.nextDouble()})}.seq.toDF("id", "vector")
+
+  def main(args: Array[String]): Unit = {
+    val obj = SparkEmbeddingKNN(randomEmbedding.as[Embedding], randomEmbedding.as[Embedding], true)
+    val matchDF = obj.getKnnByPartitionUDF(topK, partitionNum)
+    matchDF.show()
+  }
 }
